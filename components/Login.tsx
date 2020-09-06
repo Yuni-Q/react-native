6056c083938ca9887a7042a84c43e76bd637c6d7
Yuni-Q <carnotcycle2@naver.com>
2020-09-07 02:23:49 +0900

    중간 저장

---
+++ components/Login.tsx
@@ -0,0 +1,85 @@
+import * as Google from 'expo-google-app-auth';
+import React from 'react';
+import { Dimensions, Image, Linking, Text, TouchableOpacity, View, AsyncStorage } from 'react-native';
+import axios from 'axios';
+import config from '../config/config.json';
+
+interface Props {
+  setToken: (token: string) => void;
+}
+
+const Login: React.FC<Props> = ({ setToken }) => {
+  var { width, height } = Dimensions.get('window');
+  const login = async () => {
+    try {
+      const googleData = await Google.logInAsync({
+        clientId: config.clientId,
+        scopes: ['profile', 'email'],
+      });
+      if (googleData.type === 'success') {
+        /* `accessToken` is now valid and can be used to get data from the Google API with HTTP requests */
+        const result = await axios.post(
+          'https://moti.company/api/v1/signin',
+          { snsType: 'google' },
+          {
+            headers: { Authorization: googleData.accessToken },
+          },
+        );
+        const token = result.data.data.accessToken;
+        await AsyncStorage.setItem('token', token).then(() => setToken(token));
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <View style={{ display: 'flex', alignItems: 'center', width, height }}>
+      <Image
+        source={require('../assets/images/motiLogo.png')}
+        style={{ width: width / 2, height: width / 2 / 5, marginTop: 204 }}
+      />
+      <Text
+        style={{
+          color: '#f1dbcd',
+          marginTop: 24,
+          fontSize: 14,
+          lineHeight: 26,
+          textAlign: 'center',
+        }}
+      >
+        Make Own True Identity
+      </Text>
+      <View style={{ position: 'absolute', bottom: 100, display: 'flex', alignItems: 'center' }}>
+        <TouchableOpacity
+          onPress={() => {
+            login();
+          }}
+          style={{
+            backgroundColor: '#fff',
+            width: width - 16 - 16,
+            height: 44,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 20,
+            marginTop: 24,
+          }}
+        >
+          <View style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', marginRight: 8 }}>
+            <Image source={require('../assets/images/icApple.png')} style={{ width: 22, height: 22 }} />
+            <Text style={{ fontSize: 14 }}>Sign in with google</Text>
+          </View>
+        </TouchableOpacity>
+        <Text style={{ marginTop: 24, color: '#d4a17d' }}>By creating an account you are agreeing to</Text>
+        <TouchableOpacity
+          onPress={() => Linking.openURL('https://www.notion.so/MOTI-35d01dd331bb4aa0915c33d28d60b63c')}
+        >
+          <Text style={{ marginTop: 8, color: '#d4a17d', textDecorationLine: 'underline' }}>MOTI's User Agreement</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,51 +1,44 @@
-import React from "react";
-import {
-  AsyncStorage,
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
-import Error from "./Error";
+import moment from 'moment';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import Error from './Error';
 
 const Main = () => {
-  AsyncStorage.setItem("key", "savingData");
-  AsyncStorage.getAllKeys();
-  AsyncStorage.getAllKeys((err, keys) => {
-    AsyncStorage.multiGet(keys, (err, stores) => {
-      stores.map((result, i, store) => {
-        // get at each store's key/value so you can work with it
-        let key = store[i][0];
-        let value = store[i][1];
-      });
-    });
-  });
+  // AsyncStorage.setItem("key", "savingData");
+  // AsyncStorage.getAllKeys();
+  // AsyncStorage.getAllKeys((err, keys) => {
+  //   AsyncStorage.multiGet(keys, (err, stores) => {
+  //     stores.map((result, i, store) => {
+  //       // get at each store's key/value so you can work with it
+  //       let key = store[i][0];
+  //       let value = store[i][1];
+  //     });
+  //   });
+  // });
 
-  AsyncStorage.mergeItem("dbKey", "savingData");
-  AsyncStorage.clear();
+  // AsyncStorage.mergeItem("dbKey", "savingData");
 
   return (
     <>
       <View
         style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          width: "100%"
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          width: '100%',
         }}
       >
-        {[1, 2, 3, 4, 5, 6].map(num => {
+        {[1, 2, 3, 4, 5, 6].map((num) => {
           return (
             <View key={num}>
-              <Text style={{ color: "#d4a17d" }}>{num}th</Text>
+              <Text style={{ color: '#d4a17d' }}>{num}th</Text>
               <View
                 style={{
                   width: 16,
                   height: 16,
                   borderRadius: 16,
-                  backgroundColor: "#d4a17d",
-                  marginTop: 8
+                  backgroundColor: '#d4a17d',
+                  marginTop: 8,
                 }}
               />
             </View>
@@ -55,42 +48,24 @@ const Main = () => {
       <Error internet />
       <View
         style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          width: "100%"
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          width: '100%',
         }}
       >
         <View>
-          <Image
-            source={require("../assets/images/normal.png")}
-            style={{ width: 24, height: 24 }}
-          />
+          <Image source={require('../assets/images/normal.png')} style={{ width: 24, height: 24 }} />
         </View>
         <View>
-          <Text style={{ color: "#d4a17d", fontSize: 20, lineHeight: 36 }}>
-            Nov. 2nd week
-          </Text>
+          <Text style={{ color: '#d4a17d', fontSize: 20, lineHeight: 36 }}>{moment().format('YYYY. MM. DD')}</Text>
         </View>
         <View>
-          <Image
-            source={require("../assets/images/normal.png")}
-            style={{ width: 24, height: 24 }}
-          />
+          <Image source={require('../assets/images/icProfileToucharea.png')} style={{ width: 24, height: 24 }} />
         </View>
       </View>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    color: "#fff",
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "space-between"
-  }
-});
 
 export default Main;
